880d21940978631b5c4858c8e262a30316e44fbc
Shahmeer Khan <kshahmeer10@gmail.com>
2024-03-31 20:22:55 -0700

    yes

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 #! /user/bin/env node
 import inquirer from "inquirer";
+import chalk from "chalk";
 //copmuter will generate a rendom number
 //user input a guess number
 //compare user input with computer generated number then show result
+console.clear;
+console.log(chalk.blue.bold("\n \t This is Number Guessing Game\t"));
 const rendom_number = Math.floor(Math.random() * 6 + 1);
 const answer = await inquirer.prompt([
     {
@@ -13,10 +16,10 @@ const answer = await inquirer.prompt([
 ]);
 console.log(answer);
 if (answer.guess_number === rendom_number) {
-    console.log(" Congratulation! \n You guess a right number");
+    console.log(chalk.green(" \n \t Congratulation! \n You guess a right number"));
 }
 else {
-    console.log(" Unfortunatly! You guess a wrong number" +
-        " \nThe correct number is = " +
-        rendom_number);
+    console.log(chalk.red(" \n \tUnfortunatly! You guess a wrong number\n") +
+        chalk.green(" \n \tThe correct number is = " +
+            rendom_number));
 }
